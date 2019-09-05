09f077440812ca9d8e1327c64eda52be3dba0ae1
Deepak Govindarajan <deepak.g@krds.fr>
2019-09-05 14:55:53 +0530

    firebase cloud messaging testing worked

--- android/app/src/main/java/com/knowledgeupdateapp/MainApplication.java
+++ android/app/src/main/java/com/knowledgeupdateapp/MainApplication.java
@@ -4,6 +4,9 @@ import android.app.Application;
 
 import com.facebook.react.PackageList;
 import com.facebook.react.ReactApplication;
+import io.invertase.firebase.RNFirebasePackage;
+import io.invertase.firebase.messaging.RNFirebaseMessagingPackage;                       
+import io.invertase.firebase.notifications.RNFirebaseNotificationsPackage;
 import com.dieam.reactnativepushnotification.ReactNativePushNotificationPackage;
 import com.agontuk.RNFusedLocation.RNFusedLocationPackage;
 import com.airbnb.android.react.maps.MapsPackage;
@@ -45,6 +48,8 @@ public class MainApplication extends Application implements ReactApplication {
       // Packages that cannot be autolinked yet can be added manually here, for example:
       // packages.add(new MyReactNativePackage());
       packages.add(new FBSDKPackage(mCallbackManager)); // Register Facebook SDK package with the Callback Manager
+      packages.add(new RNFirebaseMessagingPackage());
+      packages.add(new RNFirebaseNotificationsPackage());
       return packages;
     }
 
